d7cd0cd74d97a218883c28b6505eaf6e011cc763
Aruni Mishra <arunimishramsit@gmail.com>
2021-08-14 20:31:34 +0530

    Primitive

--- 1. First Steps/HelloWorld/src/mishra/aruni/Primitive.java
+++ 1. First Steps/HelloWorld/src/mishra/aruni/Primitive.java
@@ -1,4 +1,35 @@
 package mishra.aruni;
 
+import java.math.BigDecimal;
+
 public class Primitive {
+    public static void main(String[] args) {
+
+        int myMinInteger = Integer.MIN_VALUE;
+        System.out.println(myMinInteger);
+
+        int a = 1_2_3;
+        System.out.println(a);
+
+        byte myMinByte= Byte.MIN_VALUE;
+        System.out.println(myMinByte);
+
+        short myShortByte = Short.MIN_VALUE;
+        System.out.println(myShortByte);
+
+        long myLong = 100L;
+        long myMaxValue= Long.MAX_VALUE;
+        System.out.println(myMaxValue);
+
+        //casting
+        byte myNewByte = (byte) (myMinByte/1);
+
+        double myMinDouble = Double.MIN_VALUE;
+        System.out.println(myMinDouble);
+
+        BigDecimal myBiGBigDecimal = BigDecimal.TEN;
+
+        System.out.println(myBiGBigDecimal);
+
+    }
 }
