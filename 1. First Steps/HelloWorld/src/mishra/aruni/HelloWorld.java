c83b89be1cc0ec5934b560bc4eb884645d5afbc3
Aruni Mishra <arunimishramsit@gmail.com>
2021-08-24 16:34:43 +0530

    Point

--- 1. First Steps/HelloWorld/src/mishra/aruni/HelloWorld.java
+++ 1. First Steps/HelloWorld/src/mishra/aruni/HelloWorld.java
@@ -4,8 +4,8 @@ import java.util.Arrays;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("hello World");
-        System.out.println("args = " + Arrays.deepToString(args));
+//        System.out.println("hello World");
+//        System.out.println("args = " + Arrays.deepToString(args));
 
 //        SpeedConverter speedConverter = new SpeedConverter();
 //        speedConverter.printConversion(95.75);
@@ -15,7 +15,15 @@ public class HelloWorld {
 //        System.out.println(NumberOfDaysInMonth.getDaysInMonth(12,10000));
 
         //System.out.println(SumDigit.sumDigits(91345));
-        System.out.println( SumFirstAndLastDigit.sumFirstAndLastDigit(23251));
+        //System.out.println( SumFirstAndLastDigit.sumFirstAndLastDigit(23251));
+
+        Point first = new Point(6, 5);
+        Point second = new Point(3, 1);
+        System.out.println("distance(0,0)= " + first.distance());
+        System.out.println("distance(second)= " + first.distance(second));
+        System.out.println("distance(2,2)= " + first.distance(2, 2));
+        Point point = new Point();
+        System.out.println("distance()= " + point.distance());
 
 
     }
